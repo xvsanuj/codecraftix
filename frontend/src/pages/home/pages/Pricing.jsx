db20db2b5aee75dda3b9ab6d35317e9ec40da260
xvsanuj <anuj.shar2k8@gmail.com>
2024-12-22 14:10:28 +0530

    white page transformed

--- frontend/src/pages/home/pages/Pricing.jsx
+++ frontend/src/pages/home/pages/Pricing.jsx
@@ -1,7 +1,7 @@
 const Pricing = () => {
     return (
         <div>
-            <div className='h-screen w-full bg-zinc-300'>
+            <div className='h-screen w-full'>
                 <div className='w-full flex flex-col items-center justify-center pt-32'>
                     <h1 className='font-bold text-center text-6xl lg:text-9xl uppercase leading-none flex items-center'>Pricing</h1>
                     <p className='lg:text-lg text-sm bebas tracking-widest' style={{ wordSpacing: "3.4vw" }}>minimal prices are listed.</p>
--- frontend/src/pages/home/pages/Workflow.jsx
+++ frontend/src/pages/home/pages/Workflow.jsx
@@ -1,7 +1,7 @@
 const Workflow = () => {
     return (
         <div>
-            <div className='h-screen w-full bg-zinc-300'>
+            <div className='h-screen w-full'>
                 <div className='w-full flex flex-col items-center justify-center pt-32'>
                     <h1 className='font-bold text-center text-5xl lg:text-9xl uppercase leading-none flex items-center'>Workflow</h1>
                     <p className='lg:text-lg text-xs bebas tracking-widest' style={{ wordSpacing: "3.4vw" }}>our working process is listed here.</p>
